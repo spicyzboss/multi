469b540ba7d90ae6d7da5885041ca7d6519a7ea9
spicyzboss <bossthth1209mine@gmail.com>
2021-04-28 13:48:54 +0700

    :pencil2: add: title

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 // Draft ไง!!!!
+const HEIGHT = window.innerHeight
+const WIDTH = window.innerWidth
+
 print = (a) => {
     console.log(a)
+}
+
+isInViewport = (elem) => {
+    const rect = elem.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (HEIGHT || document.documentElement.clientHeight) &&
+        rect.right <= (WIDTH || document.documentElement.clientWidth)
+    );
 }
 
 // const building1 = document.querySelector(".building-1")
 // print(building1)
 
-const HEIGHT = window.innerHeight
-const WIDTH = window.innerWidth
+// const sub_1 = document.querySelector("h1")
 
-const sub_1 = document.querySelector(".sub-1")
-
-top = sub_1.getBoundingClientRect().top
-bot = sub_1.getBoundingClientRect().bottom
+// top = sub_1.getBoundingClientRect().top
+// bot = sub_1.getBoundingClientRect().bottom
 document.addEventListener('scroll', () => {
-    print(`${HEIGHT} ${WIDTH}`)
+    // print(isInViewport(sub_1))
+    // print(`${HEIGHT} ${WIDTH}`)
+    // print(`${light.getBoundingClientRect().top} ${light.getBoundingClientRect().bottom}`)
 })
 
 // building1.style.filter = `brightness(50%)`
