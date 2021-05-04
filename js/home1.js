a68845e9c4ba5633b95e1cbe1ef3ac4d3daaa2ec
kaiwin50 <73681213+kaiwin50@users.noreply.github.com>
2021-05-04 21:43:27 +0700

    Add files via upload

---
+++ js/home1.js
@@ -0,0 +1,49 @@
+home1.innerHTML = `
+<g transform="scale(0.5) translate(-220 -240)">
+<!--พื้น-->
+<g transform="translate(-130 -20) skewY(-26.5) skewX(45)">
+    <rect width="40" height="52" x="27" y="146" fill="#91a7ab"/>
+</g>
+<!--ข้าง1-->
+<g transform="skewY(-26.5)">
+    <rect width="35" height="25" x="95" y="85.5" fill="#0f5f82"/>
+    <rect width="5" height="12" x="103" y="92" stroke="#b5e6fb"fill="#4fcee6"/>
+    <rect width="5" height="12" x="119" y="92" stroke="#b5e6fb"fill="#4fcee6"/>
+    <rect width="3" height="20" x="48.9" y="44.8" fill="#fbffff"/>
+</g>
+<!--หน้า1-->
+<g transform="skewY(26.5)">
+    <rect width="25" height="25" x="70" y="-9" fill="#478eb2"/>
+    <rect width="15" height="15" x="55" y="-9" fill="#0f5f82"/>
+    <rect width="5" height="12" x="73" y="-4" stroke="#b5e6fb"fill="#4fcee6"/>
+    <rect width="5" height="12" x="85" y="-4" stroke="#b5e6fb"fill="#4fcee6"/>
+</g>
+<!--เสาหน้า-->
+<g transform="skewY(26.5)">
+    <rect width="3" height="25" x="46" y="-9" fill="#f2fcfa"/>
+</g>
+<!--หน้า2-->
+<g transform="skewY(26.5)">
+    <rect width="5" height="12" x="64" y="-6.4" stroke="#eff5f4"fill="#8ecfda"/>
+    <rect width="25" height="7" x="45.5" y="-9" fill="#478eb2"/>
+</g>
+<!--บน1-->
+<g transform="skewY(26.5) skewX(-45)">
+    <rect width="52" height="40" x="35" y="-48" fill="#c0ced7"/>
+</g>
+<!--หน้า3-->
+<g transform="skewY(26.5)">
+    <rect width="52" height="2" x="43" y="-8" fill="white"/>
+</g>
+<!--ข้าง2-->
+<g transform="skewY(-26.5)">
+    <rect width="40" height="2" x="95" y="86.5" fill="#649aae"/>
+</g>
+<!--หลังคา1-->
+<g transform="skewY(26.5) skewX(-30)">
+    <rect width="49" height="40" x="40" y="-50" fill="#49464f"/>
+</g>
+<!--หลังคา2-->
+<path d="M95 37 l22 -28.5 l15 10" fill="#26202d"/>
+
+</g>`
---
+++ js/trees.js
@@ -0,0 +1,54 @@
+Trees.innerHTML = `
+<use href="#tree" x="250" y="260"/>
+<use href="#tree" x="305" y="285"/>
+<use href="#tree" x="-15" y="110"/>
+<use href="#tree" x="-55" y="188"/>
+
+<use href="#tree" x="15" y="220"/>
+<use href="#tree" x="-81" y="112"/>
+<use href="#tree" x="180" y="160"/>
+<use href="#tree" x="191" y="184"/>
+<use href="#tree" x="115" y="189"/>
+<use href="#tree" x="92" y="139"/>
+<use href="#tree" x="132" y="167"/>
+<use href="#tree" x="138" y="135"/>
+<use href="#tree" x="180" y="120"/>
+<use href="#tree" x="227" y="125"/>
+<use href="#tree" x="255 " y="145"/>
+<use href="#tree" x="5" y="175"/>
+<use href="#tree" x="30" y="135"/>
+<use href="#tree" x="-55" y="142"/>
+<use href="#tree" x="267" y="117"/>
+<use href="#tree" x="287" y="130"/>
+<use href="#tree" x="445" y="108"/>
+<use href="#tree" x="470" y="120"/>
+<use href="#tree" x="490" y="131"/>
+<use href="#tree" x="509" y="118"/>
+<use href="#tree" x="513" y="143"/>
+<use href="#tree" x="427" y="178"/>
+<use href="#tree" x="560" y="110"/>
+
+<use href="#tree" x="359" y="147"/>
+<use href="#tree" x="528" y="108"/>
+<use href="#tree" x="560" y="110"/>
+<use href="#tree" x="545" y="118"/>
+<use href="#tree" x="566" y="129"/>
+<use href="#tree" x="531" y="132"/>
+<use href="#tree" x="538" y="156"/>
+<use href="#tree" x="554" y="144"/>
+<use href="#tree" x="487" y="106"/>
+<use href="#tree" x="576" y="154"/>
+<use href="#tree" x="558.5" y="168.5"/>
+<use href="#tree" x="576" y="178"/>
+<use href="#tree" x="565" y="250"/>
+<use href="#tree" x="579" y="119"/>
+
+<use href="#tree" x="400" y="335"/>
+<use href="#tree" x="520" y="395"/>
+<use href="#tree" x="390" y="250"/>
+<use href="#tree" x="410" y="240"/>
+<use href="#tree" x="430" y="230"/>
+<use href="#tree" x="510" y="310"/>
+<use href="#tree" x="570" y="340"/>
+
+`
