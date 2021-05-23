ec34281c052efaec1a3df5bfba7fb641ed7b140a
thatchon <71114727+thatchon@users.noreply.github.com>
2021-05-23 19:51:34 +0700

    Update app.js

--- app.js
+++ app.js
@@ -25,3 +25,31 @@ window.addEventListener("load", () => {
         epr_thai3.style.opacity = 100;
     }, 5700);
 })
+
+/**
+ * Back to top button
+ */
+ const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+        return [...document.querySelectorAll(el)]
+    } else {
+        return document.querySelector(el)
+    }
+}
+const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+let backtotop = select('.back-to-top')
+if (backtotop) {
+    const toggleBacktotop = () => {
+        if (window.scrollY > 100) {
+            backtotop.classList.add('active')
+        } else {
+            backtotop.classList.remove('active')
+        }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+}
