398604416347b7fb537e05ed8de0955b9abef82e
Ultz <73681213+kaiwin50@users.noreply.github.com>
2021-05-24 18:07:00 +0700

    Add files via upload

--- js/system/coin.js
+++ js/system/coin.js
@@ -3,15 +3,15 @@ let my_curent_Coin = 500, my_curent_Fame = 0,
   produced = 0, bad_fame = 0, all_returned = 0, produce_state=1, fame_make_money;
 
 let per_Sec = setInterval(()=>{
-    if(my_curent_Fame < 100){
+    if(100-Math.round(bad_fame*100/my_curent_Fame) < 25){
         return_package_per_sec = Math.floor(Math.random()*4)/10;
         fame_make_money = 1;
     }
-    else if(my_curent_Fame < 300){
+    else if(100-Math.round(bad_fame*100/my_curent_Fame) < 40){
         return_package_per_sec = (Math.floor(Math.random()*(6-3))+3)/10;
         fame_make_money = 1.2;
     }
-    else if(my_curent_Fame < 700){
+    else if(100-Math.round(bad_fame*100/my_curent_Fame) < 80){
         return_package_per_sec = (Math.floor(Math.random()*(8-5))+5)/10;
         fame_make_money = 1.5;
     }
@@ -28,7 +28,7 @@ let per_Sec = setInterval(()=>{
     produced += (4+2*fac2_inuse)*produce_state;
     bad_fame += bad_fame_per_sec_package+bad_fame_per_sec_building;
     myCoin.innerHTML = Math.round(my_curent_Coin);
-    myFame.innerHTML = Math.round(my_curent_Fame*100)/100;
+    myFame.innerHTML = Math.round(my_curent_Fame*100);
     myStock.innerHTML = my_curent_Product;
     myReturned_package.innerHTML = Math.round(my_curent_Returned);
     cashFlow.innerHTML = Math.round(cash_per_sec_package+coin_per_sec_building);
@@ -61,7 +61,7 @@ let per_Sec = setInterval(()=>{
     else{
         bubble_recycle.dataset.state = "onHide";
     }
-}, 1000);
+}, 1);
 
 function sellProduct(){
     if(my_curent_Product >= 100){
